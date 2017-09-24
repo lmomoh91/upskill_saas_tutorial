810d0eea3163436dc191000576a4a7ce14a91bcc
Leoman Momoh <eshaiha@gmail.com>
2017-09-24 17:38:13 +0000

    Corrected Contacts Table

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,21 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message Sent!"
+    else 
+      redirect_to new_contact_path, notice: "Error!!! Message not sent."
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:Name, :email, :comments)
+    end
+  
 end
 
